1a30e3a10f0a5d04c70a4c0a1a9f2fd49a66877f
dhruvmojila <dhruvmojila098@gmail.com>
2025-11-10 03:14:00 -0500

    fix token

--- backend/app/utils/auth.py
+++ backend/app/utils/auth.py
@@ -2,9 +2,9 @@ from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
 import bcrypt
-from datetime import datetime, timedelta
 from typing import Optional
 from sqlalchemy.orm import Session
+from datetime import datetime, timedelta, timezone
 
 from ..config import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
 from .. import models, db_models
@@ -35,9 +35,9 @@ def get_password_hash(password: str) -> str:
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
     if expires_delta:
-        expire = datetime.now() + expires_delta
+        expire = datetime.now(timezone.utc) + expires_delta
     else:
-        expire = datetime.now() + timedelta(minutes=15)
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
